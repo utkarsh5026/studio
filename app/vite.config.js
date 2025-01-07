e52e651bc546659564325594c21251905c856411
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-08 02:11:00 +0530

    Enhance Vite configuration and GitHub Actions deployment
    - Added a new `vite.config.js` file to define the Vite configuration, including React plugin and path aliasing for improved module resolution.
    - Introduced `vite.config.d.ts` for TypeScript support in Vite configuration.
    - Updated `vite.config.ts` to set the base URL for deployment to "/studio/".
    - Modified the GitHub Actions workflow in `deploy.yml` to include the new environment variable `BASE_URL` for the build process.
    
    These changes improve the project's build configuration and streamline the deployment process to GitHub Pages.

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/studio/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
